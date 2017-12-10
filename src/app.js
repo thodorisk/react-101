18936192d07d20a4ff05d453ee764991f49927ae
Thodoris Karagiannis <thodoris.karagiannis@camelotglobal.com>
2017-12-11 01:54:05 +0200

    Creates simple React components & fixes ESLint errors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 import 'font-awesome/scss/font-awesome.scss';
 import 'bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './app.scss';
+
+const App = () =>
+  (
+    <div>
+      <h1 className="title">Hello from React!</h1>
+      <p>Rendering paragraph text with JSX</p>
+    </div>
+  );
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
