992086e87be350b8ee99ae463603025c0e8652f0
Thodoris Karagiannis <thodoris.karagiannis@camelotglobal.com>
2017-12-13 01:53:57 +0200

    React Props & PropTypes

--- src/introduction.js
+++ src/introduction.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const Headline = () =>
+  (
+    <h1 className="title">Welcome to the React world.</h1>
+  );
+
+const Greeting = (props) => {
+  const { name, age } = props;
+  return <p>Hello from { name }, { age }!</p>;
+};
 
 const App = () =>
   (
     <div>
-      <h1 className="title">Hello from React!</h1>
-      <p>Rendering paragraph text with JSX</p>
+      <Headline />
+      <Greeting name="Thodoris" />
     </div>
   );
+
+Greeting.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number
+};
+
+Greeting.defaultProps = {
+  age: 32
+};
 
 export default App;
