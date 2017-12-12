d3908e536b6f1af11f1aa2b819efa15718e0f8ae
Thodoris Karagiannis <thodoris.karagiannis@camelotglobal.com>
2017-12-13 00:35:09 +0200

    Import & export modules

---
+++ src/introduction.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () =>
+  (
+    <div>
+      <h1 className="title">Hello from React!</h1>
+      <p>Rendering paragraph text with JSX</p>
+    </div>
+  );
+
+export default App;
