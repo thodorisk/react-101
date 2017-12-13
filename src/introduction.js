5cd3ba8cf660e7ff0e034b1917d3bdcacfb42b16
Thodoris Karagiannis <thodoris.karagiannis@camelotglobal.com>
2017-12-13 15:27:51 +0200

    Uses state & creates a React class

--- src/introduction.js
+++ src/introduction.js
@@ -1,31 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Headline = () =>
-  (
-    <h1 className="title">Welcome to the React world.</h1>
-  );
-
-const Greeting = (props) => {
-  const { name, age } = props;
-  return <p>Hello from { name }, { age }!</p>;
-};
-
-const App = () =>
-  (
-    <div>
-      <Headline />
-      <Greeting name="Thodoris" />
-    </div>
-  );
-
-Greeting.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number
-};
-
-Greeting.defaultProps = {
-  age: 32
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buyItems: ['milk', 'sugar', 'coffee']
+    };
+  }
+  render() {
+    const { buyItems } = this.state;
+    return (
+      <div>
+        <h1>Shopping List</h1>
+        {
+          buyItems.map(item =>
+            (
+              <p key={item}>{item}</p>
+            ))
+      }
+      </div>
+    );
+  }
+}
 
 export default App;
